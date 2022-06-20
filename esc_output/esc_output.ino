0fb0facf050aa1690541109b33c884cb526eaa1f
kevinbecquet <kevinn.becquet@gmail.com>
2022-06-20 12:15:02 +0200

    can make the motor turn

--- esc_output/esc_output.ino
+++ esc_output/esc_output.ino
@@ -1,7 +1,17 @@
 
+/*
+Can make the motor turn and chage its speed according to the sent value
 
+/!\ drv_pwm_setup_freq(motorPin,freq) can't provide a reliable pwm signal under 1.1kHz
+
+
+*/
 
 int  motorPin = 12;
+
+double freq = 10000;// PWM frequency in Hz
+
+double dc = 127;  
 
 
 // linear mapping
@@ -9,60 +19,39 @@ double map(double x, double in_min, double in_max, double out_min, double out_ma
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-//PWM 
-void pwm(double period,double dc,int pin){
-    
-    digitalWrite(pin,HIGH);
-    delayMicroseconds(dc);
-    digitalWrite(pin,LOW);
-    delayMicroseconds(period - dc);   
+
+// returns the pwm output rate to send to the ESC
+double setCommand(double val){
+  
+  if(val < 0 || val > 255){
+    Serial.println("Not valid : Enter PWM signal value -1 to 1, 0 to stop");
+    return dc;
+  }
+  else{
+    //double dc = map(val,-1.0,1.0,0,255);
+    double dc = val;
+    Serial.print("Command: ");
+    Serial.println(val);
+    return dc;
+  }
 }
-
-
-
-//int freq = 1/0.003;
-double freq = 10000;// PWM frequency in Hz
-double period = 1/freq*1000000; // PWM period in us
-
-double val2 = period/2;  
 
 void setup() {
   
- Serial.begin(57600);
-
- delay(7000); // delay to allow the ESC to recognize the stopped signal
+  Serial.begin(57600);
+  drv_pwm_setup_freq(motorPin,freq);
+  delay(7000); // delay to allow the ESC to recognize the stopped signal
 }
 
 void loop() {
   
-  //change tha value if another one has been entered
   if(Serial.available()){
-           
-      String reading = Serial.readString();
-      double val = reading.toFloat(); 
-      
-      if(val < -1 || val > 1)
-        Serial.println("Not valid : Enter PWM signal value -1 to 1, 0 to stop");
-      
-      else{
-        val2 = map(val,-1.0,1.0,0,period);
-        Serial.print(" | val: ");
-        Serial.println(val);
-        
-      }
-    }
-
-    // send a PWM cycle to the esc
-    pwm(period,val2,motorPin);
     
-    Serial.print("val2: ");
-    Serial.println(val2);
-
-    
-
-   
-    
-    
-    
+    Serial.println("Serial acquisition...");
+    String reading = Serial.readString();
+    dc = setCommand(reading.toFloat());
+  }
+  
+  analogWrite(motorPin,dc);    
   
 }
